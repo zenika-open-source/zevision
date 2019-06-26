b91d0f88695dbc594332bc2ea5fc2b96b9d5b65f
Ihab Bendidi <ihabnobendidi@gmail.com>
2019-06-26 10:09:19 +0200

    fixed modularisation

--- test/test_predict.py
+++ test/test_predict.py
@@ -8,11 +8,8 @@ sys.path.insert(0, '/home/ihab/ihabgit/zevision')
 import lib.predict as predict
 imagePaths = list(paths.list_images("db_test"))
 for image in imagePaths:
-
-	response = predict.recognize_face(image)
-
-	test_image = cv2.imread(image)
-	test_image = draw_boxes(test_image,response)
+    
+    response = predict.recognize_face(image)
+    test_image = cv2.imread(image)
+    test_image = draw_boxes(test_image,response)
     save_image(image,test_image,"results/")
-
-

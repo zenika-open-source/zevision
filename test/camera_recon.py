fc9cf2e2cfa47025c9050d942c53a73f0fae9e1b
Ihab Bendidi <ihabnobendidi@gmail.com>
2019-06-26 10:27:32 +0200

    fixed typos

--- test/camera_recon.py
+++ test/camera_recon.py
@@ -1,6 +1,8 @@
-import lib.predict as predict
+
 import sys
 # Add test option where it gets the path of each laptop and inserts it here
 sys.path.insert(0, '/home/ihab/ihabgit/zevision')
+import lib.predict as predict
+
 
 predict.recognize_camera()

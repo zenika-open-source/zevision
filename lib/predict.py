275f12430538254110be1d0de8ea96e0b6c1fbb0
Ihab Bendidi <ihabnobendidi@gmail.com>
2019-07-03 05:22:23 +0200

    boxes

--- lib/predict.py
+++ lib/predict.py
@@ -417,7 +417,7 @@ def recognize(encodings, boxes,data):
 def draw_boxes(read_image,response):
     for (i,r) in enumerate(response):
         #print("\n \n the number ",i+1," prediction  is  :   ",r)
-        box = dlib.rectangle(r["box"][1], r["box"][2], r["box"][3], r["box"][0])
+        box = dlib.rectangle(r["box"][3]*255, r["box"][0]*255, r["box"][1]*255, r["box"][2]*255)
         top = box.top()
         right = box.right()
         bottom = box.bottom()

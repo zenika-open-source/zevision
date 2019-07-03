3e6ff6e9e31fdb71b72803b92048c3b63dc09c88
Ihab Bendidi <ihabnobendidi@gmail.com>
2019-07-03 05:07:55 +0200

    boxes

--- lib/predict.py
+++ lib/predict.py
@@ -187,11 +187,11 @@ def draw_bounding_box_on_image_array(image,ymin,xmin,ymax,xmax,color='red',thick
         ymin, xmin, ymax, xmax as relative to the image.  Otherwise treat
         coordinates as absolute.
         """
-        image_pil = Image.fromarray(np.uint8(image)).convert('RGB')
-        draw_bounding_box_on_image(image_pil, ymin, xmin, ymax, xmax, color,
+    image_pil = Image.fromarray(np.uint8(image)).convert('RGB')
+    draw_bounding_box_on_image(image_pil, ymin, xmin, ymax, xmax, color,
                                        thickness, display_str_list,
                                        use_normalized_coordinates)
-        np.copyto(image, np.array(image_pil))
+    np.copyto(image, np.array(image_pil))
 
 def draw_bounding_box_on_image(image,ymin,xmin,ymax,xmax,color='red',thickness=4,display_str_list=(),
                                use_normalized_coordinates=True):
@@ -216,45 +216,45 @@ def draw_bounding_box_on_image(image,ymin,xmin,ymax,xmax,color='red',thickness=4
         ymin, xmin, ymax, xmax as relative to the image.  Otherwise treat
         coordinates as absolute.
         """
-        draw = ImageDraw.Draw(image)
-        im_width, im_height = image.size
-        if use_normalized_coordinates:
-            (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
-                              ymin * im_height, ymax * im_height)
-        else:
-            (left, right, top, bottom) = (xmin, xmax, ymin, ymax)
-        draw.line([(left, top), (left, bottom), (right, bottom),
-                   (right, top), (left, top)], width=thickness, fill=color)
-        try:
-            font = ImageFont.truetype('arial.ttf', 24)
-        except IOError:
-            font = ImageFont.load_default()
+    draw = ImageDraw.Draw(image)
+    im_width, im_height = image.size
+    if use_normalized_coordinates:
+        (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                            ymin * im_height, ymax * im_height)
+    else:
+        (left, right, top, bottom) = (xmin, xmax, ymin, ymax)
+    draw.line([(left, top), (left, bottom), (right, bottom),
+                (right, top), (left, top)], width=thickness, fill=color)
+    try:
+        font = ImageFont.truetype('arial.ttf', 24)
+    except IOError:
+        font = ImageFont.load_default()
         
-        # If the total height of the display strings added to the top of the bounding
-        # box exceeds the top of the image, stack the strings below the bounding box
-        # instead of above.
-        display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
-        # Each display_str has a top and bottom margin of 0.05x.
-        total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+    # If the total height of the display strings added to the top of the bounding
+    # box exceeds the top of the image, stack the strings below the bounding box
+    # instead of above.
+    display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
+    # Each display_str has a top and bottom margin of 0.05x.
+    total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
             
-        if top > total_display_str_height:
-            text_bottom = top
-        else:
-            text_bottom = bottom + total_display_str_height
-        # Reverse list and print from bottom to top.
-        for display_str in display_str_list[::-1]:
-            text_width, text_height = font.getsize(display_str)
-            margin = np.ceil(0.05 * text_height)
-            draw.rectangle(
-                           [(left, text_bottom - text_height - 2 * margin), (left + text_width,
-                                                                             text_bottom)],
-                           fill=color)
-            draw.text(
-                      (left + margin, text_bottom - text_height - margin),
-                      display_str,
-                      fill='black',
-                      font=font)
-            text_bottom -= text_height - 2 * margin
+    if top > total_display_str_height:
+        text_bottom = top
+    else:
+        text_bottom = bottom + total_display_str_height
+    # Reverse list and print from bottom to top.
+    for display_str in display_str_list[::-1]:
+        text_width, text_height = font.getsize(display_str)
+        margin = np.ceil(0.05 * text_height)
+        draw.rectangle(
+                        [(left, text_bottom - text_height - 2 * margin), (left + text_width,
+                                                                            text_bottom)],
+                        fill=color)
+        draw.text(
+                    (left + margin, text_bottom - text_height - margin),
+                    display_str,
+                    fill='black',
+                    font=font)
+        text_bottom -= text_height - 2 * margin
 
 
 def draw_bounding_boxes_on_image(image,boxes,color='red',thickness=4,display_str_list_list=()):
@@ -273,16 +273,16 @@ def draw_bounding_boxes_on_image(image,boxes,color='red',thickness=4,display_str
         Raises:
         ValueError: if boxes is not a [N, 4] array
         """
-        boxes_shape = boxes.shape
-        if not boxes_shape:
-            return
-        if len(boxes_shape) != 2 or boxes_shape[1] != 4:
-            raise ValueError('Input must be of size [N, 4]')
-        for i in range(boxes_shape[0]):
-            display_str_list = ()
-            if display_str_list_list:
-                display_str_list = display_str_list_list[i]
-            draw_bounding_box_on_image(image, boxes[i, 0], boxes[i, 1], boxes[i, 2],boxes[i, 3], color, thickness, display_str_list)
+    boxes_shape = boxes.shape
+    if not boxes_shape:
+        return
+    if len(boxes_shape) != 2 or boxes_shape[1] != 4:
+        raise ValueError('Input must be of size [N, 4]')
+    for i in range(boxes_shape[0]):
+        display_str_list = ()
+        if display_str_list_list:
+            display_str_list = display_str_list_list[i]
+        draw_bounding_box_on_image(image, boxes[i, 0], boxes[i, 1], boxes[i, 2],boxes[i, 3], color, thickness,display_str_list)
 
 def draw_bounding_boxes_on_image_array(image,boxes,color='red',thickness=4,display_str_list_list=()):
     """Draws bounding boxes on image (numpy array).
@@ -300,9 +300,9 @@ def draw_bounding_boxes_on_image_array(image,boxes,color='red',thickness=4,displ
         Raises:
         ValueError: if boxes is not a [N, 4] array
         """
-        image_pil = Image.fromarray(image)
-        draw_bounding_boxes_on_image(image_pil, boxes, color, thickness,display_str_list_list)
-        np.copyto(image, np.array(image_pil))
+    image_pil = Image.fromarray(image)
+    draw_bounding_boxes_on_image(image_pil, boxes, color, thickness,display_str_list_list)
+    np.copyto(image, np.array(image_pil))
 ########################
 
 def draw_object_boxes(frame,response):

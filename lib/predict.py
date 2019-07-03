3b51ec3578a8dee8f901dfc2a1e8f8cf0c7a1d95
Ihab Bendidi <ihabnobendidi@gmail.com>
2019-07-03 05:05:08 +0200

    boxes

--- lib/predict.py
+++ lib/predict.py
@@ -8,6 +8,9 @@ import lib.encodings.encodings as codes
 from imutils.video import VideoStream
 import time
 from PIL import Image
+import PIL.ImageColor as ImageColor
+import PIL.ImageDraw as ImageDraw
+import PIL.ImageFont as ImageFont
 import tensorflow as tf
 
 
@@ -157,11 +160,168 @@ def recognize_objects_frame(frame):
     output_dict = run_inference_for_single_image(image_np_expanded, default_object_detector)
     
     output = organize_object_prediction(output_dict,default_object_labels)
+    
     return output
 
 
-                      
-                      
+
+#############################################################
+#############################################################
+
+def draw_bounding_box_on_image_array(image,ymin,xmin,ymax,xmax,color='red',thickness=4,display_str_list=(),
+                                     use_normalized_coordinates=True):
+    """Adds a bounding box to an image (numpy array).
+        Bounding box coordinates can be specified in either absolute (pixel) or
+        normalized coordinates by setting the use_normalized_coordinates argument.
+        Args:
+        image: a numpy array with shape [height, width, 3].
+        ymin: ymin of bounding box.
+        xmin: xmin of bounding box.
+        ymax: ymax of bounding box.
+        xmax: xmax of bounding box.
+        color: color to draw bounding box. Default is red.
+        thickness: line thickness. Default value is 4.
+        display_str_list: list of strings to display in box
+        (each to be shown on its own line).
+        use_normalized_coordinates: If True (default), treat coordinates
+        ymin, xmin, ymax, xmax as relative to the image.  Otherwise treat
+        coordinates as absolute.
+        """
+        image_pil = Image.fromarray(np.uint8(image)).convert('RGB')
+        draw_bounding_box_on_image(image_pil, ymin, xmin, ymax, xmax, color,
+                                       thickness, display_str_list,
+                                       use_normalized_coordinates)
+        np.copyto(image, np.array(image_pil))
+
+def draw_bounding_box_on_image(image,ymin,xmin,ymax,xmax,color='red',thickness=4,display_str_list=(),
+                               use_normalized_coordinates=True):
+    """Adds a bounding box to an image.
+        Bounding box coordinates can be specified in either absolute (pixel) or
+        normalized coordinates by setting the use_normalized_coordinates argument.
+        Each string in display_str_list is displayed on a separate line above the
+        bounding box in black text on a rectangle filled with the input 'color'.
+        If the top of the bounding box extends to the edge of the image, the strings
+        are displayed below the bounding box.
+        Args:
+        image: a PIL.Image object.
+        ymin: ymin of bounding box.
+        xmin: xmin of bounding box.
+        ymax: ymax of bounding box.
+        xmax: xmax of bounding box.
+        color: color to draw bounding box. Default is red.
+        thickness: line thickness. Default value is 4.
+        display_str_list: list of strings to display in box
+        (each to be shown on its own line).
+        use_normalized_coordinates: If True (default), treat coordinates
+        ymin, xmin, ymax, xmax as relative to the image.  Otherwise treat
+        coordinates as absolute.
+        """
+        draw = ImageDraw.Draw(image)
+        im_width, im_height = image.size
+        if use_normalized_coordinates:
+            (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                              ymin * im_height, ymax * im_height)
+        else:
+            (left, right, top, bottom) = (xmin, xmax, ymin, ymax)
+        draw.line([(left, top), (left, bottom), (right, bottom),
+                   (right, top), (left, top)], width=thickness, fill=color)
+        try:
+            font = ImageFont.truetype('arial.ttf', 24)
+        except IOError:
+            font = ImageFont.load_default()
+        
+        # If the total height of the display strings added to the top of the bounding
+        # box exceeds the top of the image, stack the strings below the bounding box
+        # instead of above.
+        display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
+        # Each display_str has a top and bottom margin of 0.05x.
+        total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+            
+        if top > total_display_str_height:
+            text_bottom = top
+        else:
+            text_bottom = bottom + total_display_str_height
+        # Reverse list and print from bottom to top.
+        for display_str in display_str_list[::-1]:
+            text_width, text_height = font.getsize(display_str)
+            margin = np.ceil(0.05 * text_height)
+            draw.rectangle(
+                           [(left, text_bottom - text_height - 2 * margin), (left + text_width,
+                                                                             text_bottom)],
+                           fill=color)
+            draw.text(
+                      (left + margin, text_bottom - text_height - margin),
+                      display_str,
+                      fill='black',
+                      font=font)
+            text_bottom -= text_height - 2 * margin
+
+
+def draw_bounding_boxes_on_image(image,boxes,color='red',thickness=4,display_str_list_list=()):
+    """Draws bounding boxes on image.
+        Args:
+        image: a PIL.Image object.
+        boxes: a 2 dimensional numpy array of [N, 4]: (ymin, xmin, ymax, xmax).
+        The coordinates are in normalized format between [0, 1].
+        color: color to draw bounding box. Default is red.
+        thickness: line thickness. Default value is 4.
+        display_str_list_list: list of list of strings.
+        a list of strings for each bounding box.
+        The reason to pass a list of strings for a
+        bounding box is that it might contain
+        multiple labels.
+        Raises:
+        ValueError: if boxes is not a [N, 4] array
+        """
+        boxes_shape = boxes.shape
+        if not boxes_shape:
+            return
+        if len(boxes_shape) != 2 or boxes_shape[1] != 4:
+            raise ValueError('Input must be of size [N, 4]')
+        for i in range(boxes_shape[0]):
+            display_str_list = ()
+            if display_str_list_list:
+                display_str_list = display_str_list_list[i]
+            draw_bounding_box_on_image(image, boxes[i, 0], boxes[i, 1], boxes[i, 2],boxes[i, 3], color, thickness, display_str_list)
+
+def draw_bounding_boxes_on_image_array(image,boxes,color='red',thickness=4,display_str_list_list=()):
+    """Draws bounding boxes on image (numpy array).
+        Args:
+        image: a numpy array object.
+        boxes: a 2 dimensional numpy array of [N, 4]: (ymin, xmin, ymax, xmax).
+        The coordinates are in normalized format between [0, 1].
+        color: color to draw bounding box. Default is red.
+        thickness: line thickness. Default value is 4.
+        display_str_list_list: list of list of strings.
+        a list of strings for each bounding box.
+        The reason to pass a list of strings for a
+        bounding box is that it might contain
+        multiple labels.
+        Raises:
+        ValueError: if boxes is not a [N, 4] array
+        """
+        image_pil = Image.fromarray(image)
+        draw_bounding_boxes_on_image(image_pil, boxes, color, thickness,display_str_list_list)
+        np.copyto(image, np.array(image_pil))
+########################
+
+def draw_object_boxes(frame,response):
+    image_np = load_frame_into_numpy_array(frame)
+    for r in response :
+        draw_bounding_box_on_image_array(image_np,r['box'][0],r['box'][1],r['box'][2],r['box'][3],display_str_list=(r['category']))
+
+
+
+
+
+
+
+
+
+
+
+
+
                       #End of object recognition
 ####################################################################################################################
 ####################################################################################################################
@@ -270,19 +430,19 @@ def draw_boxes(read_image,response):
     return read_image
 
 
-def draw_object_boxes(read_image,response):
-    for (i,r) in enumerate(response):
+#def draw_object_boxes(read_image,response):
+#   for (i,r) in enumerate(response):
         #print("\n \n the number ",i+1," prediction  is  :   ",r)
         #box = dlib.rectangle(r["box"][3], r["box"][0], r["box"][1], r["box"][2])
-        top = r["box"][3]
-        right = r["box"][0]
-        bottom = r["box"][1]
-        left = r["box"][2]
-        cv2.rectangle(read_image, (left, top), (right, bottom), (255, 0, 0), 2)
-        y = top - 15 if top - 15 > 15 else top + 15
-        cv2.putText(read_image, r["category"], (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (255, 0, 0), 2)
-    return read_image
+        #       top = r["box"][3]
+        #        right = r["box"][0]
+        #      bottom = r["box"][1]
+        #      left = r["box"][2]
+        #      cv2.rectangle(read_image, (left, top), (right, bottom), (255, 0, 0), 2)
+        #     y = top - 15 if top - 15 > 15 else top + 15
+        #     cv2.putText(read_image, r["category"], (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+        #               0.75, (255, 0, 0), 2)
+# return read_image
 
 
 
@@ -361,7 +521,7 @@ def recognize_camera (src=0,method="hog",encoding_path=default_path_encodings,re
         faces = recognize_frame(frame)
         objects = recognize_objects_frame(frame)
         frame = draw_boxes(frame,faces)
-        frame = draw_object_boxes(frame,objects)
+        draw_object_boxes(frame,objects)
         cv2.imshow("Frame", frame)
         # Write the video in a the zevision/test/results/
         if record_path != None:

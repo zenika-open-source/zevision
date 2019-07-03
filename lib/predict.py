f83ddc9d3d7da5f79a319e07c4288494099b703b
ihab <ihabnobendidi@gmail.com>
2019-07-03 03:21:40 +0200

    test

--- lib/predict.py
+++ lib/predict.py
@@ -55,6 +55,7 @@ def detection_method(method):
 
 
 def load_image_into_numpy_array(image):
+    print(image.size)
     (im_width, im_height) = image.size
     return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
 
